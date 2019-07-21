de9c99d682c69b2135b8bcc6674af0e2b4a13edb
Jason Howie <howiejason93@gmail.com>
2019-07-21 13:31:36 -0400

    create appendUsers

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,25 @@ function getUsers() {
 
 getUsers();
 
+/**
+ * Loops through the data and appends
+ * the data according to the template
+ * @param {object} data - data from the API call
+ */
 function appendUsers(data) {
     console.log(data)
+    const people = data.map(item => {
+        const html = `<div class="card">
+                        <div class="card-img-container">
+                            <img class="card-img" src="${item.picture.large}" alt="${item.name.first} ${item.name.last}">
+                        </div>
+                        <div class="card-info-container">
+                            <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
+                            <p class="card-text">${item.email}</p>
+                            <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
+                        </div>
+                    </div>`;
+        return html;
+    }).join('');
+    gallery.innerHTML = people;
 }
