f3134a246dc5d7d043dd4ed3a967348ec8c5fc0e
Jason Howie <howiejason93@gmail.com>
2019-07-21 14:30:39 -0400

    create listenForCLick

--- js/scripts.js
+++ js/scripts.js
@@ -22,19 +22,36 @@ getUsers();
  * @param {object} data - data from the API call
  */
 function appendUsers(data) {
-    console.log(data)
-    const people = data.map(item => {
-        const html = `<div class="card">
+    const people = data.map((item, index) => {
+        const html = `<div class="card" id="${item.name.first}${item.name.last}">
                         <div class="card-img-container">
                             <img class="card-img" src="${item.picture.large}" alt="${item.name.first} ${item.name.last}">
                         </div>
                         <div class="card-info-container">
                             <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
                             <p class="card-text">${item.email}</p>
-                            <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
+                            <p class="card-text cap">${item.location.city}, ${item.location.state} ${item.nat}</p>
                         </div>
                     </div>`;
+        listenForClick(item);
         return html;
     }).join('');
     gallery.innerHTML = people;
+}
+
+function createModal(data) {
+    console.log(data);
+}
+
+/**
+ * 
+ * @param {object} data - data from API call 
+ */
+function listenForClick(data) {
+    setTimeout(event => {
+        const person = document.getElementById(`${data.name.first}${data.name.last}`);
+        person.addEventListener('click', e => {
+            createModal(data);
+        })
+    },100)
 }
